b2288104a3753b62db2808b528659629da8318eb
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-05-23 01:25:22 +0300

    add workflow build

---
+++ common.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Source: https://github.com/sec/dotnet-core-freebsd-source-build/blob/v8/common.sh
+
+set -e
+
+function calculate_build_id()
+{
+    local TAG=$1
+    local REGEX='-(alpha|preview|rc|rtm)\.[0-9]\.([0-9]{5})\.([0-9]{1,2})'
+
+    if [[ $TAG =~ $REGEX ]]
+    then
+        local MAJOR=${BASH_REMATCH[2]}
+        local MINOR=${BASH_REMATCH[3]}
+
+        YEAR=$((MAJOR / 1000 + 2000))
+        MONTH=$(((MAJOR % 1000) / 50))
+        DAY=$((MAJOR % 50))
+
+        DATE=$((YEAR * 10000 + MONTH * 100 + DAY))
+
+        OFFICIALBUILDID="${DATE}.${MINOR}"
+    else
+        OFFICIALBUILDID=$(date +%Y%m%d).99
+    fi
+    echo $OFFICIALBUILDID
+}
